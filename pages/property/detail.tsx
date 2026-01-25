8d7e57829101791f9aef133678184a3a2dcaa269
Ray-pusan <raygithomework@gmail.com>
2026-01-25 22:01:40 +0900

    feat: integration Header and Footer via Layouts

---
+++ pages/property/detail.tsx
@@ -0,0 +1,8 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container } from "@mui/material";
+
+const PropertyDetail = () => {
+  return <Container>Property detail page</Container>;
+};
+
+export default withLayoutBasic(PropertyDetail);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,5 +1,13 @@
-const Property = () => {
-  return <div>Property</div>;
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return (
+    <>
+      <Container>Property list</Container>
+    </>
+  );
 };
 
-export default Property;
+export default withLayoutBasic(PropertyList);
