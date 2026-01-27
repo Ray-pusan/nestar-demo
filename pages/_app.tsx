422c84c547947e6628859f5c86a19ffce7496d0b
Ray-pusan <raygithomework@gmail.com>
2026-01-27 19:48:01 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { light } from "@/scss/MaterialTheme";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import "../scss/app.scss"
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
