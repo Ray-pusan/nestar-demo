f80658e0c2e87fbd8b127dfe3af9adaa3b924364
Ray-pusan <raygithomework@gmail.com>
2026-02-02 11:41:31 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,20 @@ import { useState } from "react";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
 
-  // Socket.io redux, Mui ........
+  // Socket.io redux, Mui apollo........
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
